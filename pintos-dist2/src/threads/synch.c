79fcbe3dc95a1a27ec8ded0f84b00658f5579a0e
Harman Singh <harman.singh@hotmail.co.nz>
2017-05-04 23:08:05 +1200

    Made priority donate semaphore work
    2 tests remaining to pass.
    To make the priority donate semaphore work. I had to sort the list when performing a semaphore up. This updates the waiters of the semaphore to reflect any changes in priority of the threads such as due to priority donation.

--- pintos-dist2/src/threads/synch.c
+++ pintos-dist2/src/threads/synch.c
@@ -116,9 +116,11 @@ sema_up (struct semaphore *sema)
   ASSERT (sema != NULL);
 
   old_level = intr_disable ();
-  if (!list_empty (&sema->waiters))
+  if (!list_empty (&sema->waiters)){
+  	list_sort(&sema->waiters, thread_priority_comparator, NULL);
     thread_unblock (list_entry (list_pop_front (&sema->waiters),
                                 struct thread, elem));
+  }
   sema->value++;
   thread_yield_check();
   intr_set_level (old_level);
